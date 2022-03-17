cbad039f9393736373e6227c12e1ab6e9548d232
saeed.at <saeed.alijani@aut.ac.ir>
2022-03-17 17:29:35 +0330

    Create session3_getstart.cpp

---
+++ Session 3/cpp/session3_getstart.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+/*
+/ 1. add two variable a and b and print result
+int main(int argc, char** argv) {
+	
+	int a, b, sum;
+	
+	a = 1;
+	b = 2;
+	
+	sum = a + b;
+	
+	cout<<sum;
+
+	return 0;
+}
+*/
+
+/*
+/ 2. take two number from user and the print the sum of two number
+int main(int argc, char** argv) {
+	
+	int a, b, sum;
+	
+	cout<<"enter a:\n";
+	cin>>a;
+	
+	cout<<"enter b:\n";
+	cin>>b;
+	
+	sum = a + b;
+	
+	cout<<"result :" << sum;
+
+	return 0;
+}
+*/
+
+/*
+// 3. take a number from user and if it was bigger than 100 print
+//  bigger than 100 otherwise print less than 100 else equal to 100
+int main(int argc, char** argv) {
+	
+	int a;
+	
+	cout<<"Give me the number:\n";
+	cin>>a;
+	
+	if (a>100){
+		cout<<"Bigger than 100";
+	}
+	else if(a<100){
+		cout<<"Less than 100";
+	}
+	else{
+		cout<<"Equal to 100";
+	}
+
+	return 0;
+}
+*/
+
+/*
+// 4. print 10 hellow with for loop
+int main(int argc, char** argv){
+	
+	for(int i =0;i<10;i++) 
+	{
+		cout<<"Hello\n";
+	}
+	return 0;
+}
+*/
+
+/*
+// 5. print 10 hellow with while loop
+int main(int argc, char** argv) {
+	int i = 0;
+	while(i<10)
+	{
+		cout<<"Hello\n";
+		i++;
+	}
+}
+*/
+
+/*
+// 6. print sum of the numbers from 1 to 10
+int main(int argc, char** argv) {
+	int sum = 0;
+	int i;
+	for (i=1;i<11;i++)
+	{
+		sum+=i;
+	}
+	cout<<"Result : "<<sum;
+	}
+*/
